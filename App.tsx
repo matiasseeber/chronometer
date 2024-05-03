dad8cbaddeddbea1c11a35b71f5e1d7edb7d733c
JuanIgnacioBellavitis <juan.bellavitis@gmail.com>
2024-05-03 13:16:23 -0300

    Navegacion

--- App.tsx
+++ App.tsx
@@ -1,21 +1,81 @@
-import { StyleSheet, View } from 'react-native';
-import Chronometer from './src/views/chronometer';
-import Timer from './src/views/timer';
+import { StyleSheet, Text, View } from "react-native";
+import Chronometer from "./src/views/chronometer";
+import Timer from "./src/views/timer";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
+
+const screenOptions = {
+  tabBarShowLabel: false,
+  headerShown: false,
+  tabBarStyle: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    left: 0,
+    elevation: 0,
+    height: 60,
+    backgroundColor: "#121212",
+  },
+};
 
 export default function App() {
   //AGREGAR UN MAIN ACA CON LA NAVEGACION PARA CADA VISTA
   return (
-    <View style={styles.container}>
-      {/* <Chronometer /> */}
-      <Timer />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={screenOptions}>
+        <Tab.Screen
+          name="Chronometer"
+          component={Chronometer}
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return (
+                <View
+                  style={{ alignItems: "center", justifyContent: "center" }}
+                >
+                  <MaterialIcons
+                    name="timer"
+                    size={24}
+                    color={focused ? "#d88e20" : "#383838"}
+                  />
+                  <Text style={{ color: "#7c7c7c" }}>Chronómetro</Text>
+                </View>
+              );
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Timer"
+          component={Timer}
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return (
+                <View
+                  style={{ alignItems: "center", justifyContent: "center" }}
+                >
+                  <Ionicons
+                    name="timer"
+                    size={24}
+                    color={focused ? "#d88e20" : "#383838"}
+                  />
+                  <Text style={{ color: "#7c7c7c" }}>Temporizador</Text>
+                </View>
+              );
+            },
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
-    paddingTop: 50
-  }
+    backgroundColor: "black",
+    paddingTop: 50,
+  },
 });
--- src/views/timer.tsx
+++ src/views/timer.tsx
@@ -1,108 +1,107 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
 // import CountDown from '../components/countdown';
-import TimeText from '../components/timeText';
-import TwoButtons from '../components/twoButtons';
-import colors from '../resources/colors';
+import TimeText from "../components/timeText";
+import TwoButtons from "../components/twoButtons";
+import colors from "../resources/colors";
 
 interface TimerProps {
-    TrailWidth?: number;
-    onComplete?: () => void;
+  TrailWidth?: number;
+  onComplete?: () => void;
 }
 
 const btnFontSize = 17;
 const btnFontDiameter = 100;
 
 let startBtnInfo = {
-    backgroundColor: colors.darkGreen,
-    textColor: colors.lightGreen,
-    text: "Iniciar"
+  backgroundColor: colors.darkGreen,
+  textColor: colors.lightGreen,
+  text: "Iniciar",
 };
 
 const Timer: React.FC<TimerProps> = ({ TrailWidth = 8, onComplete }) => {
-    const [isRunning, setIsRunning] = useState(false);
-    const [timer, setTimer] = useState(60);
-    const [key, setKey] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [timer, setTimer] = useState(60);
+  const [key, setKey] = useState(0);
 
-    const onClickInitBtn = () => {
-        const newValue: boolean = !isRunning;
-        if (newValue) {
-            startBtnInfo.backgroundColor = colors.darkRed;
-            startBtnInfo.textColor = colors.lightRed;
-            startBtnInfo.text = "Detener";
-        } else {
-            startBtnInfo.backgroundColor = colors.darkGreen;
-            startBtnInfo.textColor = colors.lightGreen;
-            startBtnInfo.text = "Iniciar";
-        }
-        setIsRunning(newValue);
+  const onClickInitBtn = () => {
+    const newValue: boolean = !isRunning;
+    if (newValue) {
+      startBtnInfo.backgroundColor = colors.darkRed;
+      startBtnInfo.textColor = colors.lightRed;
+      startBtnInfo.text = "Detener";
+    } else {
+      startBtnInfo.backgroundColor = colors.darkGreen;
+      startBtnInfo.textColor = colors.lightGreen;
+      startBtnInfo.text = "Iniciar";
     }
+    setIsRunning(newValue);
+  };
 
-    const onClickLeftBtn = () => {
-        setIsRunning(false);
-        setKey(prevKey => prevKey + 1);
-        startBtnInfo.backgroundColor = colors.darkGreen;
-        startBtnInfo.textColor = colors.lightGreen;
-        startBtnInfo.text = "Iniciar";
-    }
+  const onClickLeftBtn = () => {
+    setIsRunning(false);
+    setKey((prevKey) => prevKey + 1);
+    startBtnInfo.backgroundColor = colors.darkGreen;
+    startBtnInfo.textColor = colors.lightGreen;
+    startBtnInfo.text = "Iniciar";
+  };
 
-    return (
-        <View style={styles.container}>
-            <CountdownCircleTimer
-                isPlaying={isRunning}
-                duration={timer}
-                initialRemainingTime={timer}
-                colors="#f78801"
-                trailStrokeWidth={TrailWidth}
-                strokeWidth={TrailWidth}
-                onUpdate={(time) => console.log(time)}
-                size={300}
-                onComplete={onComplete}
-                key={key}
-            >
-                {
-                    ({ remainingTime }) => <TimeText time={remainingTime} fontSize={60} />
-                }
-            </CountdownCircleTimer>
-            <View style={styles.TwoButtonsContainer}>
-                <TwoButtons
-                    leftBtnDisabled={timer == 0}
-                    leftBtnBckColor={colors.darkGray}
-                    leftBtnTextColor={colors.lightGray}
-                    leftBtnText={"Cancelar"}
-                    leftBtnDiameter={btnFontDiameter}
-                    leftBtnFontSize={btnFontSize}
-                    leftBtnOnClick={onClickLeftBtn}
-                    rightBtnDisabled={false}
-                    rightBtnBckColor={startBtnInfo.backgroundColor}
-                    rightBtnTextColor={startBtnInfo.textColor}
-                    rightBtnText={startBtnInfo.text}
-                    rightBtnDiameter={btnFontDiameter}
-                    rightBtnFontSize={btnFontSize}
-                    rightBtnOnClick={onClickInitBtn}
-                />
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <CountdownCircleTimer
+        isPlaying={isRunning}
+        duration={timer}
+        initialRemainingTime={timer}
+        colors="#f78801"
+        trailStrokeWidth={TrailWidth}
+        strokeWidth={TrailWidth}
+        onUpdate={(time) => console.log(time)}
+        size={300}
+        onComplete={onComplete}
+        key={key}
+      >
+        {({ remainingTime }) => <TimeText time={remainingTime} fontSize={60} />}
+      </CountdownCircleTimer>
+      <View style={styles.TwoButtonsContainer}>
+        <TwoButtons
+          leftBtnDisabled={timer == 0}
+          leftBtnBckColor={colors.darkGray}
+          leftBtnTextColor={colors.lightGray}
+          leftBtnText={"Cancelar"}
+          leftBtnDiameter={btnFontDiameter}
+          leftBtnFontSize={btnFontSize}
+          leftBtnOnClick={onClickLeftBtn}
+          rightBtnDisabled={false}
+          rightBtnBckColor={startBtnInfo.backgroundColor}
+          rightBtnTextColor={startBtnInfo.textColor}
+          rightBtnText={startBtnInfo.text}
+          rightBtnDiameter={btnFontDiameter}
+          rightBtnFontSize={btnFontSize}
+          rightBtnOnClick={onClickInitBtn}
+        />
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        display: "flex",
-        padding: "10%",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100%",
-        width: "100%"
-    },
-    TwoButtonsContainer: {
-        paddingTop: "5%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        width: "100%"
-    }
+  container: {
+    display: "flex",
+    padding: "10%",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    width: "100%",
+    backgroundColor: "black",
+  },
+  TwoButtonsContainer: {
+    paddingTop: "5%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
 });
 
 export default Timer;
